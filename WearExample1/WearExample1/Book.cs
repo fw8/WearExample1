256b378cd445e44efef99697988d76cf9e11a208
Florian Wolpert <wolpert@freinet.de>
2019-04-08 17:56:17 +0200

    we don't need "this." here

--- WearExample1/WearExample1/Book.cs
+++ WearExample1/WearExample1/Book.cs
@@ -13,22 +13,22 @@ namespace WearExample1
         public Book()
         {
             // create list
-            this.Pages = new List<Page>();
+            Pages = new List<Page>();
 
             //0
-            this.Pages.Add(new Page() { PageType = PageTypeEnum.Frage, Text = "Hat es geschmeckt?", NextIfNo = 2, NextIfYes = 1 });
+            Pages.Add(new Page() { PageType = PageTypeEnum.Frage, Text = "Hat es geschmeckt?", NextIfNo = 2, NextIfYes = 1 });
 
             //1 geschmeckt / ja - mehr?
-            this.Pages.Add(new Page() { PageType = PageTypeEnum.Frage, Text = "OK, möchtest Du mehr?", NextIfNo = 2, NextIfYes = 0 });
+            Pages.Add(new Page() { PageType = PageTypeEnum.Frage, Text = "OK, möchtest Du mehr?", NextIfNo = 2, NextIfYes = 0 });
 
             //2 geschmeckt / nein - nachtisch?
-            this.Pages.Add(new Page() { PageType = PageTypeEnum.Frage, Text = "Schade, trotzdem noch Nachtisch?", NextIfNo = 4, NextIfYes = 3 });
+            Pages.Add(new Page() { PageType = PageTypeEnum.Frage, Text = "Schade, trotzdem noch Nachtisch?", NextIfNo = 4, NextIfYes = 3 });
 
             //3 nachtisch ja?
-            this.Pages.Add(new Page() { PageType = PageTypeEnum.Anweisung, Text = "Alles klar, guten Appetit!", NextIfNo = 0, NextIfYes = 0 });
+            Pages.Add(new Page() { PageType = PageTypeEnum.Anweisung, Text = "Alles klar, guten Appetit!", NextIfNo = 0, NextIfYes = 0 });
 
             //4 nachtisch2?
-            this.Pages.Add(new Page() { PageType = PageTypeEnum.Anweisung, Text = "Schade!", NextIfNo = 0, NextIfYes = 0 });
+            Pages.Add(new Page() { PageType = PageTypeEnum.Anweisung, Text = "Schade!", NextIfNo = 0, NextIfYes = 0 });
         }
 
         public Page GetCurrentPage()
