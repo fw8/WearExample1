17d81eecfc778e130c1a61549f8bfda6a887aa1d
Florian Wolpert <wolpert@freinet.de>
2019-04-11 20:27:35 +0200

    Distinguish between question and answer views

--- WearExample1/WearExample1/MainActivity.cs
+++ WearExample1/WearExample1/MainActivity.cs
@@ -29,40 +29,63 @@ namespace WearExample1
             SetContentView(Resource.Layout.activity_main);
 
             losGehts = FindViewById<Button>(Resource.Id.btnLosGehts);
+            losGehts.Click += LosGehts;
 
-
-            losGehts.Click += delegate
-            {
-                SetContentView(Resource.Layout.janein);
-                frage = FindViewById<TextView>(Resource.Id.frageTxt);
-                frage.Text = book.GetCurrentPage().text;
-                FindViewById<Button>(Resource.Id.jaBtn).Click += JaClick;
-                FindViewById<Button>(Resource.Id.neinBtn).Click += NeinClick;
-            };
-
-            //SetAmbientEnabled();
+            SetAmbientEnabled();
         }
 
+        void LosGehts(object sender, EventArgs e)
+        {
+            SetContentView(Resource.Layout.janein);
+            frage = FindViewById<TextView>(Resource.Id.frageTxt);
+            frage.Text = book.GetCurrentPage().text;
+            FindViewById<Button>(Resource.Id.jaBtn).Click += JaClick;
+            FindViewById<Button>(Resource.Id.neinBtn).Click += NeinClick;
+        }
 
         void JaClick(object sender, EventArgs e)
         {
             // Handle Ja
-            SetContentView(Resource.Layout.janein);
-            frage = FindViewById<TextView>(Resource.Id.frageTxt);
-            frage.Text = book.GetNextPage(YesNoEnum.Yes).text;
-            FindViewById<Button>(Resource.Id.jaBtn).Click += JaClick;
-            FindViewById<Button>(Resource.Id.neinBtn).Click += NeinClick;
+            Page page = book.GetNextPage(YesNoEnum.Yes);
+            if (page.pageType == PageTypeEnum.Frage)
+            {
+                SetContentView(Resource.Layout.janein);
+                FindViewById<TextView>(Resource.Id.frageTxt).Text = page.text;
+                FindViewById<Button>(Resource.Id.jaBtn).Click += JaClick;
+                FindViewById<Button>(Resource.Id.neinBtn).Click += NeinClick;
+            }
+            else
+            {
+                SetContentView(Resource.Layout.activity_main);
+                FindViewById<TextView>(Resource.Id.antwortTxt).Text = page.text;
+                losGehts = FindViewById<Button>(Resource.Id.btnLosGehts);
+                losGehts.Text = "noch mal von Vorne";
+                book.currentPage = 0;
+                losGehts.Click += LosGehts;
+            }
         }
 
 
         void NeinClick(object sender, EventArgs e)
         {
             // Handle nein
-            SetContentView(Resource.Layout.janein);
-            frage = FindViewById<TextView>(Resource.Id.frageTxt);
-            frage.Text = book.GetNextPage(YesNoEnum.No).text;
-            FindViewById<Button>(Resource.Id.jaBtn).Click += JaClick;
-            FindViewById<Button>(Resource.Id.neinBtn).Click += NeinClick;
+            Page page = book.GetNextPage(YesNoEnum.No);
+            if (page.pageType == PageTypeEnum.Frage)
+            {
+                SetContentView(Resource.Layout.janein);
+                FindViewById<TextView>(Resource.Id.frageTxt).Text = page.text;
+                FindViewById<Button>(Resource.Id.jaBtn).Click += JaClick;
+                FindViewById<Button>(Resource.Id.neinBtn).Click += NeinClick;
+            }
+            else
+            {
+                SetContentView(Resource.Layout.activity_main);
+                FindViewById<TextView>(Resource.Id.antwortTxt).Text = page.text;
+                losGehts = FindViewById<Button>(Resource.Id.btnLosGehts);
+                losGehts.Text = "noch mal von Vorne";
+                book.currentPage = 0;
+                losGehts.Click += LosGehts;
+            }
         }
     }
 }
